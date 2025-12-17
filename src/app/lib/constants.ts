2476499ec8485f224947127ff79366699cc262fd
emmajane1313 <emma.jane@digitalax.xyz>
2025-12-16 21:31:06 -0500

    sodras

--- src/app/lib/constants.ts
+++ src/app/lib/constants.ts
@@ -250,7 +250,7 @@ export const RUNWAYD_VIDEOS: Record<Language, string> = {
 };
 export const CAFES_VIDEOS: Record<Language, string> = {
   en: "QmSDbkdsXuqtG1Yku8bYr6xkfFr4mhNSahkyQ9cY1HeqBN",
-  es: "QmSdyjNiKHWoNHTHhtNoTDqssuDQhNwcYEqSvWJepo3Fmn",
+  es: "QmUeiz7kzHoC8MgMUeU2S5UkA5xs7MqKCw7tRBwL1ErMef",
   pt: "QmeF49DNxK2e54E5rNvaNWHnLvQUVuJQyfMjppS92x5aPh",
   ar: "QmUoMLKjTtoCzbTvJw289ke9sJk3SynmPi3mH1H8rtNbbA",
   fa: "QmVLPvQWbEWurTnWXSnJBoiLJFijZjj3hAXwJRNYWbrDPZ",
